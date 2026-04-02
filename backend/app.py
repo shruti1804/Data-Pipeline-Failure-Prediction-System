930776b4aaa64b60c9c0daf7615dd8aa77799d2b
Shruti <swadnerkar18@gmail.com>
2026-04-02 13:53:42 +0530

    Prepared backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import joblib
+import os
 import numpy as np
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -281,4 +282,4 @@ def upload_predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
