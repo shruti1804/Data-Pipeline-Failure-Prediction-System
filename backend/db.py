5b0a3c14222ebab24cbc8920a7c1143f832e5510
Shruti <swadnerkar18@gmail.com>
2026-04-02 14:41:45 +0530

    resolved connection issue

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,12 @@
-import psycopg2
-
 def get_connection():
-    conn = psycopg2.connect(
-        dbname="pipeline_db",
-        user="postgres",
-        password="system",
-        host="localhost",
-        port="5432"
-    )
-    return conn
+    try:
+        import psycopg2
+        return psycopg2.connect(
+            dbname="pipeline_db",
+            user="postgres",
+            password="your_password",
+            host="localhost",
+            port="5432"
+        )
+    except:
+        return None
