ff12d0f8a4febb1bf59ae9de043ac2cabe2ca823
Shruti <swadnerkar18@gmail.com>
2026-04-02 22:42:56 +0530

    Fix db connection function

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,12 @@
 import os
 import psycopg2
 
-conn = psycopg2.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT")
-)
+def get_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
+    return conn
